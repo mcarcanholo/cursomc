1c9643813643abef0232aa4c7906ac6e851f21ed
Alexandre Carcanholo <mcarcanholo@hotmail.com>
2020-02-23 08:50:21 -0300

    Projeto criado

---
+++ src/test/java/com/elabbora/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elabbora.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
