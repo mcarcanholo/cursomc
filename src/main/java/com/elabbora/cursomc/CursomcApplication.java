ee610bfae817b4050b2d5edb12779f969310cf13
Alexandre Carcanholo <mcarcanholo@hotmail.com>
2020-02-27 23:20:42 -0300

    Estado e Cidade - One to Many e Many to One

--- src/main/java/com/elabbora/cursomc/CursomcApplication.java
+++ src/main/java/com/elabbora/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.elabbora.cursomc.domain.Categoria;
+import com.elabbora.cursomc.domain.Cidade;
+import com.elabbora.cursomc.domain.Estado;
 import com.elabbora.cursomc.domain.Produto;
 import com.elabbora.cursomc.repositories.CategoriaRepository;
+import com.elabbora.cursomc.repositories.CidadeRepository;
+import com.elabbora.cursomc.repositories.EstadoRepository;
 import com.elabbora.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepositoy;
+	
+	@Autowired
+	private EstadoRepository estadoRepositoy;
+	
+	@Autowired
+	private CidadeRepository cidadeRepositoy;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,10 +50,25 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+		p3.getCategorias().addAll(Arrays.asList(cat1));		
+			
 		categoriaRepositoy.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepositoy.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		//Tem que seguir obrigatoriamente a sequencia de interdependencia (primento o lado OneToMany)
+		estadoRepositoy.saveAll(Arrays.asList(est1, est2));
+		cidadeRepositoy.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 	 
 	
---
+++ src/main/java/com/elabbora/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.elabbora.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.elabbora.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+
+}
