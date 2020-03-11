25591da64716838890f4648eebbdb58ca8445746
Alexandre Carcanholo <mcarcanholo@hotmail.com>
2020-03-10 22:07:48 -0300

    Trabalhando com paginação e parametros opcionais endpoint. Show

--- src/main/java/com/elabbora/cursomc/resourcers/CategoriaResource.java
+++ src/main/java/com/elabbora/cursomc/resourcers/CategoriaResource.java
@@ -7,11 +7,13 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -59,4 +61,20 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());  
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	//Quando executado no PostMan, o page traz varias informações importantes sobre as paginas http://localhost:8080/categorias/page
+	//http://localhost:8080/categorias/page?linesPerPage=3
+	//http://localhost:8080/categorias/page?linesPerPage=3&page=1
+	//http://localhost:8080/categorias/page?linesPerPage=3&page=1&direction=DESC
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, //Se não informar, por padrão assumirá a pagina 0
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, //ordena por nome se não for informado
+			@RequestParam(value="direction", defaultValue="ASC") String direction) { //ou desc		
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);	
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));  
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 }
