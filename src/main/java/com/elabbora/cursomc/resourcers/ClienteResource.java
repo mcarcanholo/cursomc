32d55b754a8aca2985abf05a60f186e6e44bad2e
Alexandre Carcanholo <mcarcanholo@hotmail.com>
2020-03-11 21:04:49 -0300

    PUT, DELETE e GET para cliente. Put tratado para trazer dados não alterados

--- src/main/java/com/elabbora/cursomc/resourcers/ClienteResource.java
+++ src/main/java/com/elabbora/cursomc/resourcers/ClienteResource.java
@@ -1,16 +1,22 @@
 package com.elabbora.cursomc.resourcers;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elabbora.cursomc.domain.Cliente;
+import com.elabbora.cursomc.dto.ClienteDTO;
 import com.elabbora.cursomc.services.ClienteService;
 
 @RestController
@@ -25,6 +31,38 @@ public class ClienteResource {
 		
 		Cliente obj = service.find(id);		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj =  service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {//Void porque quando eu apagar retorno resposta com corpo vazio
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {		
+		List<Cliente> list = service.findAll();	
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, //Se não informar, por padrão assumirá a pagina 0
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, //ordena por nome se não for informado
+			@RequestParam(value="direction", defaultValue="ASC") String direction) { //ou desc		
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);	
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));  
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/elabbora/cursomc/services/ClienteService.java
+++ src/main/java/com/elabbora/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.elabbora.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.elabbora.cursomc.domain.Cliente;
+import com.elabbora.cursomc.domain.Cliente;
+import com.elabbora.cursomc.dto.ClienteDTO;
 import com.elabbora.cursomc.repositories.ClienteRepository;
+import com.elabbora.cursomc.services.exception.DataIntegrityException;
 import com.elabbora.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -21,4 +29,42 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	//Necessário implementar desta forma para que o update mantenha os campos que não foram alterados,
+	//caso contrário fica null
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque existem entidades relacionadas!!!");
+			
+		}		
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	//Fazendo a paginação
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	//Método aulixiar para instanciar uma Cliente a partir do DTO
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
 }
